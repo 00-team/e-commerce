1985df498e18529526294a4025664b4c609cc4bf
sadra <sadrataghavi1383@gmail.com>
2021-05-27 12:34:15 +0430

    linked site to views and DB to frontend

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def item_list(request):
+    context = {
+        "items" : Item.objects.all()
+    }
+    return render(request,"pages/home-page.html",context)
+
+
+def checkout(request):
+    return render(request, "pages/checkout-page.html")
+
+
+def product(request):
+    return render(request, "pages/product-page.html")
