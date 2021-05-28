5deea97ffb899dccc442179f742cab5ee9711c20
sadra <sadrataghavi1383@gmail.com>
2021-05-28 17:34:35 +0430

    worked on fuinctionality of add to card method and debugged some of it

--- Main/urls.py
+++ Main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",HomeView.as_view(),name="item-list"),
     path("checkout/", checkout, name="checkout"),
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
+    path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import *
+from django.utils import timezone
 from .models import *
 
 # Create your views here.
@@ -14,8 +15,49 @@ class ItemDetailView(DetailView):
     template_name = "pages/product.html"
     context_object_name = "items"
 
-
-
 def checkout(request):
-    return render(request, "pages/checkout-page.html")
+    return render("pages/checkout-page.html")
+
+# def add_to_cart(request,slug):
+#     item = get_object_or_404(Item,slug=slug)
+#     order_item = OrderItem.objects.create(item=item)
+#     order_qs = Order.objects.filter(user=request.user,ordered=False)
+#     if order_qs.exists():
+#         order = order_qs[0]
+#         if order.items.filter(item__slug=item.slug).exists():
+#             order_item.quautity += 1
+#             order_item.save()
+#     else:
+#         ordered_date = timezone.now()
+#         order = Order.objects.create(
+#             user=request.user, ordered_date=ordered_date)
+#         order.items.add(order_item)
+#     return redirect("product", slug=slug)
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+        )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+
+    return redirect("product", slug=slug)
+
+    
 
