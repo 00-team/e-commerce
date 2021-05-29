b75b1a0f2ae76b73c84a95c8b229112fdafce9fb
sadra <sadrataghavi1383@gmail.com>
2021-05-29 15:20:43 +0430

    url for them

--- Main/urls.py
+++ Main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
     path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
     path("remove-from-cart/<slug>/", remove_from_cart, name="remove-from-cart"),
+    path("accounts/profile/", profile, name="profile"),
 ]
