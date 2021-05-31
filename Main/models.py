c7e01ba4476085d4c9f3895874c520900993025c
sadra <sadrataghavi1383@gmail.com>
2021-06-01 02:47:49 +0430

    models function updated

--- Main/models.py
+++ Main/models.py
@@ -63,6 +63,12 @@ class OrderItem(models.Model):
 
     def get_amout_saved(self):
         return self.get_total_item_price() - self.get_total_discount_item_price()
+    
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
 
 
 class Order(models.Model):
@@ -76,3 +82,8 @@ class Order(models.Model):
     def __str__(self):
         return self.user.username
 
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
