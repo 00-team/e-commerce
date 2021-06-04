c606527c6c5b04ba2ce7ddfc6a0ad73a8cb0a334
sadra <sadrataghavi1383@gmail.com>
2021-06-04 18:01:50 +0430

    linking remove item to the ui

--- Main/urls.py
+++ Main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
     path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
     path("remove-from-cart/<slug>/", remove_from_cart, name="remove-from-cart"),
+    path("remove-single-item-from-cart/<slug>/", remove_single_from_cart, name="remove-single-from-cart"),
     path("accounts/profile/", profile, name="profile"),
 ]
