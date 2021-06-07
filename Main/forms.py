28313bb4de1bb99e5361027fd4d4477e92409d41
sadra <sadrataghavi1383@gmail.com>
2021-06-07 21:29:13 +0430

    payment methods

--- Main/forms.py
+++ Main/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django_countries.fields import CountryField
 
+PAYMENT_CHOICES = (
+    ("I","IDPay"),
+    ("P","PayPal")
+)
 class CheckoutForm(forms.Form):
     street_address = forms.CharField()
     appartment_address = forms.CharField(required=False)
@@ -8,4 +12,4 @@ class CheckoutForm(forms.Form):
     zip_address = forms.CharField()
     same_billing_addres = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect(),choices=PAYMENT_CHOICES)
