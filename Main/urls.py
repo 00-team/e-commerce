8417a6410049d06e7c9cd1f7d9f6804a305e9582
sadra <sadrataghavi1383@gmail.com>
2021-06-07 21:24:08 +0430

    linking class based view to ui

--- Main/urls.py
+++ Main/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("",HomeView.as_view(),name="item-list"),
-    path("checkout/", checkout, name="checkout"),
+    path("checkout/", CheckoutView.as_view(), name="checkout"),
     path("order-summary/", OrderSummaryView.as_view(), name="order-summary"),
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
     path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
