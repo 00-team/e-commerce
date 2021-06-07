5fdae4cd70f18a19730c12e096a0ae33fbafa824
sadra <sadrataghavi1383@gmail.com>
2021-06-07 22:31:50 +0430

    added class to country section for styling

--- Main/forms.py
+++ Main/forms.py
@@ -8,15 +8,20 @@ PAYMENT_CHOICES = (
 )
 class CheckoutForm(forms.Form):
     street_address = forms.CharField(widget=forms.TextInput(attrs={
-        "placeholder":"1234 Main St"
+        "placeholder":"1234 Main St",
+        "class": "ml-3 w-75"
     }))
     appartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
-        "placeholder": "Apartment or Suite"}))
-    country = CountryField(blank_label="").formfield(attrs={
+        "placeholder": "Apartment or Suite",
+        "class": "ml-3 w-75" 
+        }))
+    country = CountryField(blank_label="(select country)").formfield(widget=CountrySelectWidget(attrs={
         "class": "custom-select d-block w-100"
-    })
+    }))
     zip_address = forms.CharField(widget=forms.TextInput(attrs={
-        "id": "zip"}))
+        "id": "zip",
+        "class": "form-control"
+        }))
     same_billing_addres = forms.BooleanField(required=False)
     save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(widget=forms.RadioSelect(),choices=PAYMENT_CHOICES)
