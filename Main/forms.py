5a196c70e0423fbd675ba1a975d14dd313484c87
sadra <sadrataghavi1383@gmail.com>
2021-06-07 22:18:02 +0430

    worked on class adding from back-end to elements for styling and some other apies

--- Main/forms.py
+++ Main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django_countries.widgets import CountrySelectWidget
 from django_countries.fields import CountryField
 
 PAYMENT_CHOICES = (
@@ -11,8 +12,11 @@ class CheckoutForm(forms.Form):
     }))
     appartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
         "placeholder": "Apartment or Suite"}))
-    country = CountryField(blank_label="").formfield()
-    zip_address = forms.CharField()
-    same_billing_addres = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    country = CountryField(blank_label="").formfield(attrs={
+        "class": "custom-select d-block w-100"
+    })
+    zip_address = forms.CharField(widget=forms.TextInput(attrs={
+        "id": "zip"}))
+    same_billing_addres = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(widget=forms.RadioSelect(),choices=PAYMENT_CHOICES)
